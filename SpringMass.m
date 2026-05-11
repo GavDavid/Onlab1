507abcf1f1ca9c4f61ce05dad7e51cad72de007e
GavDavid <gavallerke50@gmail.com>
2026-05-12 00:26:20 +0200

    Update SpringMass.m

--- SpringMass.m
+++ SpringMass.m
@@ -27,7 +27,7 @@ a_ideal(length(t)) = -k / m * s_ideal(length(t)) - c / m * v_ideal(length(t));
 
 s_meas = s_ideal;
 
-noise_level = 0.0001;    
+noise_level = 0.00001;    
 gauss = noise_level * randn(size(s_meas));
 
 s_meas = s_meas + gauss;
@@ -40,14 +40,18 @@ a_meas = diff(v_meas);
 a_meas(length(a_meas) + 1) = a_meas(length(a_meas));
 a_meas = a_meas / dt;
 
+% s_filt = sgolayfilt(s_meas,3,21);
+% 
+% v_meas = gradient(s_filt,dt);
+% a_meas = gradient(v_meas,dt);
 
 for i = 1 : length(a_ideal)
-    Theta(i,1:6) = [1, v_meas(i), s_meas(i),...
+    Theta(i,1:10) = [1, v_meas(i), s_meas(i),...
     v_meas(i)*s_meas(i), v_meas(i)^2,...
-    s_meas(i)^2];
+    s_meas(i)^2, s_meas(i)^2*v_meas(i), s_meas(i)*v_meas(i)^2, s_meas(i)^3, v_meas(i)^3];
 end
 
-Theta_functions = {'1', 'v', 's', 'v*s', 'v^2', 's^2'};
+Theta_functions = {'1', 'v', 's', 'v*s', 'v^2', 's^2', 's^2*v', 's*v^2', 's^3', 'v^3'};
 
 RMS_theta = ones(length(a_ideal), 1) * rms(Theta);
 Theta_norm = Theta ./ RMS_theta;
@@ -62,26 +66,178 @@ for i = 1 : length(v_meas)
     x_der(i, 2) = a_meas(i);
 end
 
-Xi = inv(Theta_norm'*Theta_norm + 1.5e-2 * eye(6,6)) * Theta_norm' * x_der(:, 2);
+% Tikhonov regularizált least squares
+% Xi = inv(Theta_norm'*Theta_norm) * Theta_norm' * x_der(:, 2);
+
+% % Sparse regression 
+% lambda = 0.15;   
+% n_iter = 100;
+% 
+% for i = 1:n_iter
+% 
+%     Xi(abs(Xi) < lambda) = 0;
+% 
+%     remains = (Xi ~= 0);
+% 
+%     Theta_new = Theta_norm(:, remains);
+%     Xi_red = inv(Theta_new' * Theta_new) * Theta_new' * x_der(:, 2);
+% 
+%     Xi_new = zeros(size(Xi));
+%     Xi_new(remains) = Xi_red;
+% 
+%     Xi = Xi_new;
+% end
+
+
+% SVD felbontás inverz
+[U,S,V] = svd(Theta_norm, 'econ');
+
+% V utolsó oszlopát megnézve mivel ez a legkisebb szinguláris érték
+% látszik, hogy v^3 domináns -> kidobás
+
+
+remove_idx = 10;
+
+Theta_reduced = Theta_norm;
+Theta_reduced(:,remove_idx) = [];
+
+Theta_functions_reduced = Theta_functions;
+Theta_functions_reduced(remove_idx) = [];
+
+[U,S,V] = svd(Theta_reduced,'econ');
+
+
+% ismét V utolsó oszlopát nézve látszik, hogy az s-t tartalmazó tagok
+% dominánsak így először kidobom s^3-t
+
+remove_idx = 9;
+
+Theta_reduced(:,remove_idx) = [];
+
+Theta_functions_reduced(remove_idx) = [];
+
+[U,S,V] = svd(Theta_reduced,'econ');
+
+% s*v^2 és v*s dominánsak, eldobom ezeket
+remove_idx = [4, 8];
+
+Theta_reduced(:,remove_idx) = [];
+
+Theta_functions_reduced(remove_idx) = [];
+
+[U,S,V] = svd(Theta_reduced,'econ');
+
+% s^2 és s^2*v dominánsak, eldobom
+
+remove_idx = [5, 6];
+
+Theta_reduced(:,remove_idx) = [];
+
+Theta_functions_reduced(remove_idx) = [];
+
+[U,S,V] = svd(Theta_reduced,'econ');
+
+% v és v^2 dominánsak, de v^2 a nagyobb rendű így eldobom azt
+
+remove_idx = 4;
+
+Theta_reduced(:,remove_idx) = [];
+
+Theta_functions_reduced(remove_idx) = [];
+
+[U,S,V] = svd(Theta_reduced,'econ');
+
+V(:,end)
+% így már elég nagyok a szinguláris értékek
+active_features = true(1,length(Theta_functions));
+
+active_features(strcmp(Theta_functions,'v^3')) = false;
+active_features(strcmp(Theta_functions,'s^3')) = false;
+active_features(strcmp(Theta_functions,'v*s')) = false;
+active_features(strcmp(Theta_functions,'s*v^2')) = false;
+active_features(strcmp(Theta_functions,'s^2')) = false;
+active_features(strcmp(Theta_functions,'s^2*v')) = false;
+active_features(strcmp(Theta_functions,'v^2')) = false;
+
+Theta_reduced = Theta_norm(:,active_features);
+
+Theta_functions_reduced = Theta_functions(active_features);
+
+[U,S,V] = svd(Theta_reduced,'econ');
+
+s = diag(S);
+
+% % sima SVD
+% threshold = 1e-6 * s(1);
+% 
+% s_inv = zeros(size(s));
+% 
+% for j = 1:length(s)
+%     if s(j) > threshold
+%         s_inv(j) = 1/s(j);
+%     end
+% end
+%
+% Xi_reduced = V * diag(s_inv) * U' * x_der(:,2);
+
+% Thikonov SVD
+lambda_reg = 1e-3;
+
+s_filt = s ./ (s.^2 + lambda_reg);
+
+Xi_reduced = V * diag(s_filt) * U' * x_der(:,2);
+
+
+
+
+Xi = zeros(length(Theta_functions),1);
+
+Xi(active_features) = Xi_reduced;
+
 
 % Sparse regression 
-lambda = 0.15;   
+lambda = 0.05;   
 n_iter = 100;
 
 for i = 1:n_iter
-   
+
     Xi(abs(Xi) < lambda) = 0;
-    
+
     remains = (Xi ~= 0);
-  
+
     Theta_new = Theta_norm(:, remains);
-    Xi_red = inv(Theta_new' * Theta_new + 1.5e-2 * eye(size(Theta_new' * Theta_new))) * Theta_new' * x_der(:, 2);
+
+    [U,S,V] = svd(Theta_new,'econ');
+
+    s = diag(S);
+
+    % sima SVD
+    % threshold = 1e-6 * s(1);
+    % 
+    % s_inv = zeros(size(s));
+    % 
+    % for j = 1:length(s)
+    %     if s(j) > threshold
+    %         s_inv(j) = 1/s(j);
+    %     end
+    % end
+    % 
+    % Xi_red = V * diag(s_inv) * U' * x_der(:,2);
+
+    % Thikonov SVD
+    lambda_reg = 1e-3;
     
+    s_filt = s ./ (s.^2 + lambda_reg);
+    
+    Xi_red = V * diag(s_filt) * U' * x_der(:,2);
+
+
     Xi_new = zeros(size(Xi));
     Xi_new(remains) = Xi_red;
-    
+
     Xi = Xi_new;
 end
+
 
 Xi = Xi ./ RMS_theta(1,:)';
 
@@ -118,9 +274,10 @@ a_rec = zeros(1, length(t));
 s_rec(1) = s_meas(1);
 v_rec(1) = v_meas(1);
 for i = 1:(length(t) - 1)
-    Theta_rec(i,1:6) = [1, v_rec(i), s_rec(i),...
+    Theta_rec(i,1:10) = [1, v_rec(i), s_rec(i),...
     v_rec(i)*s_rec(i), v_rec(i)^2,...
-    s_rec(i)^2];
+    s_rec(i)^2,  s_rec(i)^2*v_rec(i), s_rec(i)*v_rec(i)^2,...
+    s_rec(i)^3, v_rec(i)^3];
 
     a_rec(i) = Theta_rec(i, :) * Xi;
     v_rec(i+1) = v_rec(i) + a_rec(i) * dt;
